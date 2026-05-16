b0e09cc88742f4bd42eca19c5b99a70c275e4af2
Georgi Grozdev <37684063+xmobg@users.noreply.github.com>
2026-05-16 21:27:11 +0300

    Refactor rack counting logic in fashion_boutique.py

--- stack_que/fashion_boutique.py
+++ stack_que/fashion_boutique.py
@@ -1,13 +1,21 @@
-clothes = list(map(int,input().split()))
+
+clothes = list(map(int, input().split()))
+
 rack_capacity = int(input())
 
-racks = 0
+
+racks_count = 1  
+current_load = 0  
 
 while clothes:
-    racks += 1
-    current_rack_capacity = rack_capacity
-    while clothes and current_rack_capacity >= clothes[-1]:
-        current_rack_capacity -= clothes.pop()
+    
+    item = clothes.pop()
 
 
-print(racks)
+    if current_load + item <= rack_capacity:
+        current_load += item
+    else:
+        racks_count += 1
+        current_load = item
+
+print(racks_count)
