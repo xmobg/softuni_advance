dce2575793c296dc7fc3cf9ef7b02f23b7465e6e
Georgi Grozdev <37684063+xmobg@users.noreply.github.com>
2026-05-16 20:37:45 +0300

    Refactor stacked_queries.py to use match-case

--- stack_que/stacked_queries.py
+++ stack_que/stacked_queries.py
@@ -2,16 +2,22 @@ n = int(input())
 
 stack = []
 
-functions = {
-    "1": lambda n: stack.append(int(n)),
-    "2": lambda : stack.pop() if stack else None,
-    "3": lambda : print(max(stack)) if stack else None,
-    "4": lambda : print(min(stack)) if stack else None,
-
-}
-
 for _ in range(n):
     query = input().split()
-    functions[query[0]](*query[1:])
 
-print(*reversed(stack), sep=", ")
+    command = query[0]
+
+    match command:
+        case "1":
+            stack.append(int(query[1]))
+        case "2":
+            if stack:
+                stack.pop()
+        case "3":
+            if stack:
+                print(max(stack))
+        case "4":
+            if stack:
+                print(min(stack))
+
+print(", ".join(map(str, reversed(stack))))
