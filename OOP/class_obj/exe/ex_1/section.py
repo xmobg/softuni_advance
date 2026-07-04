ca880caec735d370c52b1535312eb14059c5c07c
Georgi Grozdev <37684063+xmobg@users.noreply.github.com>
2026-07-04 11:00:44 +0300

    Create section.py

---
+++ OOP/class_obj/exe/ex_1/section.py
@@ -0,0 +1,32 @@
+class Section:
+    def __init__(self, name):
+        self.name = name
+        self.tasks = []
+
+    def add_task(self, new_task):
+        if new_task in self.tasks:
+            return f"Task is already in the section {self.name}"
+
+        self.tasks.append(new_task)
+        return f"Task {new_task.details()} is added to the section"
+
+    def complete_task(self, task_name):
+        for task in self.tasks:
+            if task.name == task_name:
+                task.completed = True
+                return f"Completed task {task_name}"
+
+        return f"Could not find task with the name {task_name}"
+
+    def clean_section(self):
+        count = len([t for t in self.tasks if t.completed])
+        self.tasks = [t for t in self.tasks if not t.completed]
+        return f"Cleared {count} tasks."
+
+    def view_section(self):
+        result = f"Section {self.name}:"
+
+        for task in self.tasks:
+            result += f"\n{task.details()}"
+
+        return result
